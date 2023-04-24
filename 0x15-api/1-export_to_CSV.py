b596359e0072cc062c8522addef8c45f5fdedc1d
isybliss <isyblissumunna@gmail.com>
2023-04-24 14:50:39 -0700

    pep validate

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,14 +9,16 @@ from sys import argv
 
 if __name__ == '__main__':
     userId = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(userId)
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(userId)
+    url = 'https://jsonplaceholder.typicode.com'
+    user_url = '{}/users/{}'.format(url, userId)
+    todo_url = '{}/todos?userId={}'.format(url, userId)
     user = get(user_url).json()
     todo = get(todo_url).json()
     name = user.get('username')
-    
+
     filename = userId + '.csv'
     with open(filename, 'w', newline='') as csvfile:
         taskWriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
-            taskWriter.writerow([int(userId), name, task.get('completed'), task.get('title')])
+            taskWriter.writerow([int(userId), name, task.get('completed'),
+                task.get('title')])
